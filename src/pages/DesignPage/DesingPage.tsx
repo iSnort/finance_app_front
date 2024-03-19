709cdcb0a975a2b3628021a484f3a4816486c6a9
Snort <jlopez98.dev@gmail.com>
2024-03-19 11:34:14 -0500

    RatioList Component

--- src/pages/DesignPage/DesingPage.tsx
+++ src/pages/DesignPage/DesingPage.tsx
@@ -1,3 +1,4 @@
+import RatioList from "../../Components/RatioList/RatioList"
 import Table from "../../Components/Table/Table"
 
 interface Props {}
@@ -7,6 +8,7 @@ const DesingPage = (props: Props) => {
     <>
         <h1>FinShark Design Page</h1>
         <h2>This is FinShark's design page. This is where we will house various design aspects ot the app</h2>
+        <RatioList />
         <Table />
     </>
   )
